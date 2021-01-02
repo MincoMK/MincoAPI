c7ece0b09f5f00292521754fe9816205c38034e1
MincoMK <68775735+MincoMK@users.noreply.github.com>
2021-01-02 22:27:37 +0900

    Add files via upload

---
+++ MincoIllegalAccessException.java
@@ -0,0 +1,10 @@
+package com.minco.java.exceptions;
+
+/**
+ * @author Minco
+ */
+class MincoIllegalAccessException extends MincoReflectiveOperationException {
+	public MincoIllegalAccessException(String m) {
+		super(m);
+	}
+}
---
+++ MincoReflectiveOperationException.java
@@ -0,0 +1,10 @@
+package com.minco.java.exceptions;
+
+/**
+ * @author Minco
+ */
+class MincoReflectiveOperationException extends MincoException {
+	public MincoReflectiveOperationException(String m) {
+		super(m);
+	}
+}
